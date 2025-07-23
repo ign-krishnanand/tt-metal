7df80e5bf20a7a9d121fd58e28a8c40a57bd96f8
ign-krishnanand <krishnanand.k@ignitarium.com>
2025-07-23 16:29:34 +0530

    patch embed and merge implementation

--- models/experimental/SSR/tt/__init__.py
+++ models/experimental/SSR/tt/__init__.py
@@ -1,5 +1,8 @@
 from .mlp import TTMlp
 from .window_attn import TTWindowAttention
 from .swin_transformer_block import TTSwinTransformerBlock
+from .patch_embed import TTPatchEmbed
+from .patch_merging import TTPatchMerging
+from .basic_block import TTBasicLayer
 
-__all__ = ["TTMlp", "TTWindowAttention", "TTSwinTransformerBlock"]
+__all__ = ["TTMlp", "TTWindowAttention", "TTSwinTransformerBlock", "TTPatchEmbed", "TTPatchMerging", "TTBasicLayer"]
